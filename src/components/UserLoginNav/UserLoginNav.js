e54bfa6c2044e26f0f40fa9ed710225b55c17fe1
Jessica Stephens <jeslstep1@gmail.com>
2019-02-11 20:55:56 -0600

    added material ui appbar for user login

--- src/components/UserLoginNav/UserLoginNav.js
+++ src/components/UserLoginNav/UserLoginNav.js
@@ -1,30 +1,86 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
+import Button from '@material-ui/core/Button';
 import './UserLoginNav.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+const style = {
+    root: {
+        color: '#ffffff',
+        backgroundColor: '#16233c',
+        width: 100,
+        margin: 8, 
+    },
+};
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ffffff',
+    },
+    secondary: {
+      main: '#000000',
+    },
+  },
+});
 
 
 
-const UserLoginNav = (props) => (
+function UserLoginNav(props) {
+ const { classes } = props;
+ 
+ return (
+    <MuiThemeProvider theme={theme}>
   <div>
    {props.user.id && (
         <>
-      <section>
-        <div className="welcome">
-        <h1>Welcome, { props.user.username }!</h1> 
-        </div>
-        <div className="logout">
-          <LogOutButton  />
-        </div>
-        </section>
+        <AppBar position="static">
+        <Toolbar>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+                Welcome, { props.user.username }! 
+            </Typography>
+            <Button
+              size="small"
+              style={style.root}
+              onClick={() => props.dispatch({ type: 'LOGOUT' })}
+            >
+              Log Out
+            </Button>
+         </Toolbar>
+         </AppBar>
         </>
      )}
   </div>
-);
+ </MuiThemeProvider>
+ )
+};
 
 // get user info from state
 const mapStateToProps = state => ({
   user: state.user,
 });
 
-export default connect(mapStateToProps)(UserLoginNav);
+UserLoginNav.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(connect(mapStateToProps)(UserLoginNav));
